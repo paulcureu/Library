80f3aa76bae13ae019165b00f65cb24972408d13
Paul <cureu.paul.ioan@gmail.com>
2025-05-28 18:37:56 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "http://localhost:3000",
+      "/api": "https://library-production-4143.up.railway.app/",
     },
   },
 });
